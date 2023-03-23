49d3431a8fdbfaff5bfc031b5fba35c4b1990d28
unknown <njorogekamau63@gmail.com>
2023-03-23 15:42:14 +0300

    cash or cheque

--- resource_manager/accounting/models.py
+++ resource_manager/accounting/models.py
@@ -10,13 +10,15 @@ class OperationsAccount(models.Model):
     def __str__(self):
         return self.name
     
-class Transaction(models.Model):
+class OperationsTransaction(models.Model):
     account = models.ForeignKey(OperationsAccount, on_delete=models.CASCADE)
     transaction_number = models.CharField(max_length=20)
     transaction_type = models.CharField(max_length=20)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
+    payment_type = models.CharField(max_length=10, choices=[('cash', 'Cash'), ('cheque', 'Cheque')])
 
     def __str__(self):
         return f"{self.transaction_type} - {self.amount}"
+
 
