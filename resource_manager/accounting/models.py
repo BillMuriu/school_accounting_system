2403269b2d742fa63bb2e62fb90b1b5cb4839b21
unknown <njorogekamau63@gmail.com>
2023-03-23 15:02:19 +0300

    the OperationsAccount model

--- resource_manager/accounting/models.py
+++ resource_manager/accounting/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class OperationsAccount(models.Model):
+    account_number = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
