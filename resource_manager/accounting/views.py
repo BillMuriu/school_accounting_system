ea93650bb6c1091ffbd9c843929e0168b2f7e7e9
unknown <njorogekamau63@gmail.com>
2023-03-23 19:01:29 +0300

    The updated form

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -12,26 +13,28 @@ def home(request):
 #adding money to the operation account
 def add_to_operations(request):
     if request.method == 'POST':
-        account_number = request.POST.get('account_number')
-        amount = request.POST.get('amount')
-        transaction_number = request.POST.get('transaction_number')
-        transaction_type = request.POST.get('transaction_type')
-        payment_type = request.POST.get('payment_type')
+        form = OperationsTransactionForm(request.POST)
+        if form.is_valid():
+            account_number = form.cleaned_data['account_number']
+            amount = form.cleaned_data['amount']
+            transaction_number = form.cleaned_data['transaction_number']
+            transaction_type = form.cleaned_data['transaction_type']
+            payment_type = form.cleaned_data['payment_type']
 
-        account = OperationsAccount.objects.get(account_number=account_number)
-        account.balance += float(amount)
-        account.save()
+            account = OperationsAccount.objects.get(account_number=account_number)
+            account.balance += float(amount)
+            account.save()
 
-        transaction = OperationsTransaction.objects.create(
-            account=account,
-            transaction_number=transaction_number,
-            transaction_type=transaction_type,
-            amount=float(amount),
-            payment_type=payment_type
-        )
-        transaction.save()
+            transaction = OperationsTransaction.objects.create(
+                account=account,
+                transaction_number=transaction_number,
+                transaction_type=transaction_type,
+                amount=float(amount),
+                payment_type=payment_type
+            )
+            transaction.save()
 
-        return HttpResponse(f"Added {amount} to {account_number} in {payment_type}")
+            return HttpResponse(f"Added {amount} to {account_number} in {payment_type}")
     else:
-        return render(request, 'add_to_operations.html')
-
+        form = OperationsTransactionForm()
+    return render(request, 'add_to_operations.html', {'form': form})
