6f430bd3fd9b290e19d86d8d3e25e12ff1325c92
unknown <njorogekamau63@gmail.com>
2023-03-23 17:01:27 +0300

    add_to_operations

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import OperationsAccount
+from .models import *
 
 # Create your views here.
 
@@ -8,13 +8,30 @@ def home(request):
     # your view code here
     return HttpResponse("Hello, World!")
 
-def debit_operations_account(request):
+
+#adding money to the operation account
+def add_to_operations(request):
     if request.method == 'POST':
-        account_number = request.POST['account_number']
-        amount = request.POST['amount']
+        account_number = request.POST.get('account_number')
+        amount = request.POST.get('amount')
+        transaction_number = request.POST.get('transaction_number')
+        transaction_type = request.POST.get('transaction_type')
+        payment_type = request.POST.get('payment_type')
+
         account = OperationsAccount.objects.get(account_number=account_number)
-        account.balance -= amount
+        account.balance += float(amount)
         account.save()
-        return redirect('operations_account_detail', account_number=account_number)
+
+        transaction = OperationsTransaction.objects.create(
+            account=account,
+            transaction_number=transaction_number,
+            transaction_type=transaction_type,
+            amount=float(amount),
+            payment_type=payment_type
+        )
+        transaction.save()
+
+        return HttpResponse(f"Added {amount} to {account_number} in {payment_type}")
     else:
-        return render(request, 'debit_operations_account.html')
+        return render(request, 'add_to_operations.html')
+
