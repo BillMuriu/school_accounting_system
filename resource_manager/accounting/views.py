65675b9d7b2d77d7c75ca8d6320e30fb1a8eb313
unknown <njorogekamau63@gmail.com>
2023-03-23 15:22:59 +0300

    the debit_operations_account

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import OperationsAccount
 
 # Create your views here.
 
 def home(request):
     # your view code here
     return HttpResponse("Hello, World!")
+
+def debit_operations_account(request):
+    if request.method == 'POST':
+        account_number = request.POST['account_number']
+        amount = request.POST['amount']
+        account = OperationsAccount.objects.get(account_number=account_number)
+        account.balance -= amount
+        account.save()
+        return redirect('operations_account_detail', account_number=account_number)
+    else:
+        return render(request, 'debit_operations_account.html')
