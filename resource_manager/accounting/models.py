4d0d7e6aae0c919ef30f45064c72fca0db9d9b12
unknown <njorogekamau63@gmail.com>
2023-03-23 15:29:36 +0300

    ja

--- resource_manager/accounting/models.py
+++ resource_manager/accounting/models.py
@@ -9,4 +9,14 @@ class OperationsAccount(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Transaction(models.Model):
+    account = models.ForeignKey(OperationsAccount, on_delete=models.CASCADE)
+    transaction_number = models.CharField(max_length=20)
+    transaction_type = models.CharField(max_length=20)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.transaction_type} - {self.amount}"
 
