df1bbe7be7c233a60618f30703e996eb8f0e5130
unknown <njorogekamau63@gmail.com>
2023-03-23 19:20:08 +0300

    u

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -11,3 +11,26 @@ def home(request):
 
 
 #adding money to the operation account
+def add_to_operations(request):
+    if request.method == 'POST':
+        form = OperationsTransactionForm(request.POST)
+        if form.is_valid():
+            account = form.cleaned_data['account']
+            amount = form.cleaned_data['amount']
+            payment_type = form.cleaned_data['payment_type']
+            votehead = form.cleaned_data['votehead']
+
+            account.balance += float(amount)
+            account.save()
+
+            transaction = OperationsTransaction.objects.create(
+                account=account,
+                votehead=votehead,
+                amount=float(amount),
+                payment_type=payment_type
+            )
+
+            return HttpResponse(f"Added {amount} to {account.account_number} in {payment_type}")
+    else:
+        form = OperationsTransactionForm()
+    return render(request, 'add_to_operations.html', {'form': form})
