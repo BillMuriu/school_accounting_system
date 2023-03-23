97a8df807b68690a5452cfee91f62397f67ea3a9
unknown <njorogekamau63@gmail.com>
2023-03-23 19:15:15 +0300

    adjusting the modelform

--- resource_manager/accounting/forms.py
+++ resource_manager/accounting/forms.py
@@ -4,4 +4,5 @@ from .models import OperationsTransaction
 class OperationsTransactionForm(forms.ModelForm):
     class Meta:
         model = OperationsTransaction
-        fields = ['account', 'transaction_number', 'transaction_type', 'amount', 'payment_type']
+        fields = ['account', 'votehead', 'amount', 'payment_type']
+
--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -11,30 +11,3 @@ def home(request):
 
 
 #adding money to the operation account
-# def add_to_operations(request):
-#     if request.method == 'POST':
-#         form = OperationsTransactionForm(request.POST)
-#         if form.is_valid():
-#             account_number = form.cleaned_data['account_number']
-#             amount = form.cleaned_data['amount']
-#             transaction_number = form.cleaned_data['transaction_number']
-#             transaction_type = form.cleaned_data['transaction_type']
-#             payment_type = form.cleaned_data['payment_type']
-
-#             account = OperationsAccount.objects.get(account_number=account_number)
-#             account.balance += float(amount)
-#             account.save()
-
-#             transaction = OperationsTransaction.objects.create(
-#                 account=account,
-#                 transaction_number=transaction_number,
-#                 transaction_type=transaction_type,
-#                 amount=float(amount),
-#                 payment_type=payment_type
-#             )
-#             transaction.save()
-
-#             return HttpResponse(f"Added {amount} to {account_number} in {payment_type}")
-#     else:
-#         form = OperationsTransactionForm()
-#     return render(request, 'add_to_operations.html', {'form': form})
