4e85c89be07433cf59029124973245de32494915
unknown <njorogekamau63@gmail.com>
2023-03-24 12:00:40 +0300

    new voteHead model

--- resource_manager/accounting/admin.py
+++ resource_manager/accounting/admin.py
@@ -5,5 +5,5 @@ from .models import *
 # Register your models here.
 
 admin.site.register(OperationsAccount)
-admin.site.register(Votehead)
+admin.site.register(VoteHead)
 admin.site.register(OperationsReceipt)
--- resource_manager/accounting/models.py
+++ resource_manager/accounting/models.py
@@ -40,15 +40,25 @@ class OperationsReceipt(models.Model):
 
 
 
-class Votehead(models.Model):
+class VoteHead(models.Model):
+    ACCOUNT_TYPES = (
+        ('operations', 'Operations Account'),
+        ('other', 'Other Account'),
+    )
+
     name = models.CharField(max_length=100)
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPES, default='other')
+    account_number = models.CharField(max_length=20, unique=True)
+    account = models.ForeignKey(OperationsAccount, on_delete=models.CASCADE, default=None)
+    amount_budgeted = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name
+
     
 class OperationsBudget(models.Model):
     account = models.ForeignKey(OperationsAccount, on_delete=models.CASCADE)
-    votehead = models.ForeignKey(Votehead, on_delete=models.CASCADE)
+    votehead = models.ForeignKey(VoteHead, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date_budgeted = models.DateField()
 
