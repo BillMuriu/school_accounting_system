5ff066bec5f5dce2a208fb8858c537d7bd07f5d9
unknown <njorogekamau63@gmail.com>
2023-03-25 16:06:29 +0300

    iteration 2

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -20,9 +20,48 @@ def home(request):
     # Get the Personal Emolument votehead budgeted amount
     pe_votehead = VoteHead.objects.get(name='Personal Emolument')
     pe_budgeted_amount = pe_votehead.amount_budgeted
-
+    
+    # Get the RMI and PE votehead objects
+    rmi_votehead = VoteHead.objects.get(name='Repairs Maintenance and Improvement')
+    pe_votehead = VoteHead.objects.get(name='Personal Emolument')
+    
     context = {
         'latest_receipt': latest_receipt,
+        'operations_cash_account': cash_account,
+        'repairs_maintenance_improvement': rmi_votehead,
+        'personal_emolument': pe_votehead,
+        'cash_balance': cash_balance,
+        'bank': latest_receipt.amount,
+        'rmi_budgeted_amount': rmi_budgeted_amount,
+        'pe_budgeted_amount': pe_budgeted_amount,
+    }
+
+    return render(request, 'accounting/cashbook.html', context)
+def home(request):
+    # Get the latest cheque receipt
+    latest_receipt = OperationsChequeReceipt.objects.latest('date_received')
+
+    # Get the cash account balance
+    cash_account = OperationsCashAccount.objects.first()
+    cash_balance = cash_account.cash_balance
+
+    # Get the RMI votehead budgeted amount
+    rmi_votehead = VoteHead.objects.get(name='Repairs Maintenance and Improvement')
+    rmi_budgeted_amount = rmi_votehead.amount_budgeted
+
+    # Get the Personal Emolument votehead budgeted amount
+    pe_votehead = VoteHead.objects.get(name='Personal Emolument')
+    pe_budgeted_amount = pe_votehead.amount_budgeted
+    
+    # Get the RMI and PE votehead objects
+    rmi_votehead = VoteHead.objects.get(name='Repairs Maintenance and Improvement')
+    pe_votehead = VoteHead.objects.get(name='Personal Emolument')
+    
+    context = {
+        'latest_receipt': latest_receipt,
+        'operations_cash_account': cash_account,
+        'repairs_maintenance_improvement': rmi_votehead,
+        'personal_emolument': pe_votehead,
         'cash_balance': cash_balance,
         'bank': latest_receipt.amount,
         'rmi_budgeted_amount': rmi_budgeted_amount,
@@ -31,6 +70,7 @@ def home(request):
     }
 
     return render(request, 'accounting/cashbook.html', context)
+
 
 
 
