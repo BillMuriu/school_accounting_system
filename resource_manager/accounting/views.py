ece15a6f47d971c138ff47a28ec548bc1f09caf8
unknown <njorogekamau63@gmail.com>
2023-03-25 15:47:09 +0300

    the cash_book view

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -6,7 +6,30 @@ from .forms import *
 # Create your views here.
 
 def home(request):
-    # your view code here
-    return render(request, 'accounting/cashbook.html')
+    # Get the latest cheque receipt
+    latest_receipt = OperationsChequeReceipt.objects.latest('date_received')
+
+    # Get the cash account balance
+    cash_account = OperationsCashAccount.objects.first()
+    cash_balance = cash_account.cash_balance
+
+    # Get the RMI votehead budgeted amount
+    rmi_votehead = VoteHead.objects.get(name='Repairs Maintenance and Improvement')
+    rmi_budgeted_amount = rmi_votehead.amount_budgeted
+
+    # Get the Personal Emolument votehead budgeted amount
+    pe_votehead = VoteHead.objects.get(name='Personal Emolument')
+    pe_budgeted_amount = pe_votehead.amount_budgeted
+
+    context = {
+        'latest_receipt': latest_receipt,
+        'cash_balance': cash_balance,
+        'bank': latest_receipt.amount,
+        'rmi_budgeted_amount': rmi_budgeted_amount,
+        'pe_budgeted_amount': pe_budgeted_amount,
+    }
+
+    return render(request, 'accounting/cashbook.html', context)
+
 
 
