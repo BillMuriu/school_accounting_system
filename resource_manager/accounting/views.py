4f9c818f4cf1da6a66342f94916b39dec5bad6db
unknown <njorogekamau63@gmail.com>
2023-03-25 19:50:13 +0300

    refactored

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -12,37 +12,16 @@ def home(request):
     # Get the latest cash receipt
     latest_cash_receipt = OperationsCashReceipt.objects.latest('date_received')
 
-     # Get all the votehead objects
+    # Get all the votehead objects
     voteheads = VoteHead.objects.all()
 
-    # Create a list of the votehead names
-    votehead_names = [votehead.name for votehead in voteheads]
-
-    # Get the RMI votehead budgeted amount
-    rmi_votehead = VoteHead.objects.get(name='Repairs Maintenance and Improvement')
-    rmi_budgeted_amount = rmi_votehead.amount_budgeted
-
-    # Get the Personal Emolument votehead budgeted amount
-    pe_votehead = VoteHead.objects.get(name='Personal Emolument')
-    pe_budgeted_amount = pe_votehead.amount_budgeted
-
-    # Get the RMI and PE votehead objects
-    rmi_votehead = VoteHead.objects.get(name='Repairs Maintenance and Improvement')
-    pe_votehead = VoteHead.objects.get(name='Personal Emolument')
-
     context = {
-    'latest_receipt': latest_receipt,
-    'latest_cash_receipt': latest_cash_receipt,
-    'voteheads': voteheads,
-    'votehead_names': votehead_names,
-    'repairs_maintenance_improvement': rmi_votehead,
-    'personal_emolument': pe_votehead,
-    'bank': latest_receipt.amount,
-    'rmi_budgeted_amount': rmi_budgeted_amount,
-    'pe_budgeted_amount': pe_budgeted_amount,
-    'checkreceipts': OperationsChequeReceipt.objects.all(),
-    'cashreceipts': OperationsCashReceipt.objects.all()
-}
+        'latest_receipt': latest_receipt,
+        'latest_cash_receipt': latest_cash_receipt,
+        'voteheads': voteheads,
+        'checkreceipts': OperationsChequeReceipt.objects.all(),
+        'cashreceipts': OperationsCashReceipt.objects.all()
+    }
 
     return render(request, 'accounting/cashbook.html', context)
 
