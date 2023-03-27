11fd01ced60e337f7df3dc760486a41ad6bbbe04
unknown <njorogekamau63@gmail.com>
2023-03-27 14:30:03 +0300

    adding a payee attribute

--- resource_manager/accounting/models.py
+++ resource_manager/accounting/models.py
@@ -159,6 +159,7 @@ class PaymentVoucher(models.Model):
     )
 
     date = models.DateField()
+    payee_name = models.CharField(max_length=100, default='')
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_type = models.CharField(max_length=20, choices=PAYMENT_TYPES, default='cash')
