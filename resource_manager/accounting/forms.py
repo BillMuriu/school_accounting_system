70bb25cb5736a6f0751889490ec3881da87099c0
unknown <njorogekamau63@gmail.com>
2023-04-01 13:45:40 +0300

    created the cheque receipt form

--- resource_manager/accounting/forms.py
+++ resource_manager/accounting/forms.py
@@ -1,3 +1,13 @@
 from django import forms
 from .models import *
 
+class ChequeReceiptForm(forms.ModelForm):
+    class Meta:
+        model = OperationsChequeReceipt
+        fields = ['account', 'received_from', 'amount', 'date_received']
+        widgets = {
+            'account': forms.Select(attrs={'class': 'form-control'}),
+            'received_from': forms.TextInput(attrs={'class': 'form-control'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control'}),
+            'date_received': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+        }
