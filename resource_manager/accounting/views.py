35b3214084e0b78ff35612ce0abe695fe67ce40c
unknown <njorogekamau63@gmail.com>
2023-04-03 19:45:11 +0300

    timedelta

--- resource_manager/accounting/views.py
+++ resource_manager/accounting/views.py
@@ -219,16 +219,13 @@ def cashbook(request):
 
 #What I need
 def my_view(request):
-    # Get the current month and year
-    current_month = datetime.now().month
-    current_year = datetime.now().year
-    
-    # Get the previous month and year
-    previous_month = (datetime.now().replace(day=1) - timedelta(days=1)).month
-    previous_year = datetime.now().year if previous_month != 1 else datetime.now().year - 1
-    
-    # Call the update_voteheadreceipts function to update the votehead budgets for the previous month
-    update_voteheadreceipts(previous_month, previous_year)
+    # Get the current date
+    current_date = datetime.now()
+
+    # Calculate the previous month and year
+    previous_date = current_date - timedelta(days=current_date.day)
+    previous_month = previous_date.month
+    previous_year = previous_date.year
 
     # Get all the cheque receipts for the previous month and year
     cheque_receipts = OperationsChequeReceipt.objects.filter(date_received__year=previous_year, date_received__month=previous_month)
